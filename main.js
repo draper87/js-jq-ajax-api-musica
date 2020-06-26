fbb90485c480fa87af9b6955bb3bf197a1959118
Oliver <draper87@gmail.com>
2020-06-26 16:44:49 +0200

    2

--- main.js
+++ main.js
@@ -14,7 +14,12 @@ $(document).ready(function() {
     method: 'GET',
     success: function(data) {
       var dischi = data.response; // metto in una variabile la mia array di oggetti
-      creaDischi(dischi);
+      creaDischi(dischi); // chiamo la funzione che richiama l API per generare il contenuto
+      //
+      $('button').click(function() {
+        filtroGenere(dischi);
+      })
+
     },
     error: function() {
       alert('errore');
@@ -28,6 +33,22 @@ $(document).ready(function() {
       var template = Handlebars.compile(source);
       var html = template(singoloCd); // passo a Handlebars il singolo oggetto
       $('.cds-container').append(html); // con ciclo for vado ad appendere ogni elemento contenuto nel mio array di oggetti nel html
+    }
+  }
+
+  function filtroGenere(dischi) {
+    $('.cds-container').html('');
+    for (var i = 0; i < dischi.length; i++) {
+      var singoloCd = dischi[i]; // metto in una variabile il singolo oggetto della array
+      var genereMusicale = singoloCd.genre; // prendo il genere musicale dall api
+      var genereSceltoDaUtente = $('#filtra-musica').val(); // prendo il genere musicale scelto dall utente
+      if (genereMusicale === genereSceltoDaUtente) { // solo se il genere musicale scelto dall utente coincide con quello della canzone procedo con la visualizzazione del contenuto
+        var source = $('#cd-template').html();
+        var template = Handlebars.compile(source);
+        var html = template(singoloCd);
+        $('.cds-container').append(html);
+      }
+
     }
   }
 
